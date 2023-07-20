a2815930fc6deac86be7f38d631c7f7a12d0f4b3
Abdur Rahim <abdoerrahiem@gmail.com>
2023-07-21 00:57:20 +0700

    slicing checkout

--- lib/screens/checkout_screen.dart
+++ lib/screens/checkout_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:jersipedia/utils/theme.dart';
-import 'package:jersipedia/widgets/cart_item.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
@@ -46,7 +45,175 @@ class CheckoutScreen extends StatelessWidget {
               vertical: 15,
             ),
             children: [
-              //
+              const SizedBox(height: 25),
+              Container(
+                padding: const EdgeInsets.all(15),
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: whiteColor),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Alamat: ',
+                      style: whiteTextStyle.copyWith(
+                        fontWeight: fontWeightSemiBold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Jl. Bardannadi\nDesa Pasir Panjang\nKabupaten Mempawah\nProvinsi Kalimantan Barat.',
+                      style: whiteTextStyle.copyWith(),
+                    ),
+                    const SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          foregroundColor: whiteColor,
+                          backgroundColor: Colors.transparent,
+                          padding: const EdgeInsets.symmetric(horizontal: 30),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(999),
+                            side: BorderSide(color: whiteColor, width: 1),
+                          ),
+                        ),
+                        child: Text(
+                          'Ubah Alamat',
+                          style: whiteTextStyle.copyWith(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Jumlah Harga: ',
+                            style: whiteTextStyle.copyWith(
+                              fontWeight: fontWeightSemiBold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Rp 400.000',
+                          style: whiteTextStyle.copyWith(
+                            fontWeight: fontWeightSemiBold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 25),
+                    Text(
+                      'Pilih Ekspedisi: ',
+                      style: whiteTextStyle.copyWith(
+                        fontWeight: fontWeightSemiBold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: whiteColor,
+                      ),
+                      child: DropdownButtonFormField(
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          icon: Icon(Icons.expand_more, color: blackColor),
+                          value: '1',
+                          items: [
+                            DropdownMenuItem(
+                              value: '1',
+                              child: Text(
+                                'JNE',
+                                style: blackTextStyle.copyWith(),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: '2',
+                              child: Text(
+                                'JNT',
+                                style: blackTextStyle.copyWith(),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: '3',
+                              child: Text(
+                                'Kantor Pos',
+                                style: blackTextStyle.copyWith(),
+                              ),
+                            ),
+                          ],
+                          onChanged: (value) {}),
+                    ),
+                    const SizedBox(height: 25),
+                    Text(
+                      'Biaya Ongkir: ',
+                      style: whiteTextStyle.copyWith(
+                        fontWeight: fontWeightSemiBold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Berat 0.5 kg',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Rp 15.000',
+                          style: whiteTextStyle.copyWith(
+                            fontWeight: fontWeightSemiBold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Estimasi Waktu',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '2-3 hari',
+                          style: whiteTextStyle.copyWith(
+                            fontWeight: fontWeightSemiBold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
           Positioned(
